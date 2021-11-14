6e5709afc69f10f2d373b7d6caa53add3e45520e
DannyLenk <danylenko.14@gmail.com>
2021-11-14 12:55:06 +0200

    updated readme.md, script.js
    description

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ let tail = '';
 let direction = 1;
 let intervalID = 0;
 let gameBegan = false;
+let appleIndex = 0;
 
 function renderGrid() {
    for (let i = 0; i < 100; i++) {
@@ -40,7 +41,6 @@ function moveSnake() {
    snake.unshift(snake[0] + direction);
 }
 
-
 function controlSnake(event) {
    if (event.key === 'ArrowDown' || event.key === 'Down') {
       direction = 10;
@@ -52,6 +52,17 @@ function controlSnake(event) {
       direction = 1;
    }
 }
-
 document.addEventListener('keyup', controlSnake);
 
+function showApple() {
+   do {
+      appleIndex = Math.floor(Math.random() * 100);
+   } while( grid[appleIndex].classList.contains('snake') ) {
+      grid[appleIndex].classList.add('apple');
+   }
+}
+showApple();
+
+function eatApple() {
+   
+}
